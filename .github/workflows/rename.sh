5d5d3f3960d894896b4dbc981d9e0f01a57669ef
archr-test <archr-test@github.com>
2026-08-03 16:01:15 -0300

    fix: pass target path to rename script for correct binary location
    - Add TARGET_PATH parameter to rename.sh script
    - Update workflow to pass target path with target triple
    - Fixes 'No such file or directory' when renaming binaries
    - Ensures binary is found in correct target subdirectory

--- .github/workflows/rename.sh
+++ .github/workflows/rename.sh
@@ -3,12 +3,13 @@ set -e
 
 RUNNER_OS="${1}"
 ARTIFACT_NAME="${2}"
+TARGET_PATH="${3}"
 
 if [[ "$RUNNER_OS" == "Windows" ]]; then
   # PowerShell para Windows
   # Encontrar o arquivo e renomear
-  dir target/release | findstr "archr" | awk '{print $NF}' | xargs -I {} move "target/release/{}" "$ARTIFACT_NAME"
+  dir "$TARGET_PATH/release" | findstr "archr" | awk '{print $NF}' | xargs -I {} move "$TARGET_PATH/release/{}" "$ARTIFACT_NAME"
 else
   # Bash para Linux e macOS
-  mv target/release/archr* "$ARTIFACT_NAME"
+  mv "$TARGET_PATH/release/archr*" "$ARTIFACT_NAME"
 fi
